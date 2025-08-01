2a421958232b67fbb5ef073f4a8bf338465f2262
Valerie Song <valsong88@gmail.com>
2025-07-31 21:34:56 -0700

    mentor/hacker registration page frontend

--- src/pages/HackerRegister.tsx
+++ src/pages/HackerRegister.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import logo from "../assets/logo-black.png"
+import RegistrationText from '../components/RegistrationText'
+import '../globals.css'
 
 const HackerRegister = () => {
   return (
-    <div>HackerRegister</div>
+    <>
+    <nav className="flex justify-between items-center">
+      <Link to="/"><img className="top-0 -left-50 h-30" src={logo} alt="Technica logo"></img></Link> 
+    </nav>
+    <section>
+      <h1 className="text-3xl -mt-5 mb-5">Hacker Sign Up</h1>
+      <RegistrationText name={"Name"}/>
+      <RegistrationText name={"Table #"}/>
+      <RegistrationText name={"Email"}/>
+      <RegistrationText name={"Password"}/>
+    </section>
+    <button className="mt-6 brightness-100 bg-gradient-to-r from-cyan-500 to-pink-500 brightness-125 rounded px-3 py-3 hover:brightness-100 cursor-pointer text-xl">Sign Up</button>
+    </>
   )
 }
 
