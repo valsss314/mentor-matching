bd80c6b2723bf64d77026070ab057d2403bd3136
Valerie Song <valsong88@gmail.com>
2025-09-21 18:22:27 -0400

    deleted some unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase/firebase";
-import LoginPage from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import AboutUs from "./pages/AboutUs";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import HackerRegister from "./pages/HackerRegister";
 import MentorRegister from "./pages/MentorRegister";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router"
+import { Route, Routes } from "react-router"
 import ForgotPassword from "./pages/ForgotPassword";
 import HackerScreen from "./pages/HackerScreen";
 import MentorScreen from "./pages/MentorScreen";
